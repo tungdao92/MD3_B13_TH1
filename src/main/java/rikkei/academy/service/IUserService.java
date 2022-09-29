b6a17b08f2adc4009a693c37296b97aa801fcb68
unknown <tungdao923@gmail.com>
2022-09-29 14:59:30 +0700

    up

---
+++ src/main/java/rikkei/academy/service/IUserService.java
@@ -0,0 +1,23 @@
+package rikkei.academy.service;
+
+import rikkei.academy.model.User;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IUserService {
+    void insertUser(User user) throws SQLException;
+
+    User selectUser(int id);
+
+    List<User> selectAllUsers();
+
+    boolean deleteUser(int id) throws SQLException;
+
+    boolean updateUser(User user) throws SQLException;
+
+    User getUserById(int id);
+
+    void insertUserStore(User user) throws SQLException;
+
+}
